710f91f5ec9ecf9618d38a31c4229cb748bada4b
Tagwa-Esam <tagwaesam5@gmail.com>
2018-10-12 16:54:02 -0400

    Add PropTypes package to verify ListContacts props

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 //  class ListContacts extends Component {
 //   render() {
 //
@@ -41,6 +43,11 @@ function ListContacts (props) {
       ))}
     </ol>
   )
+}
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
 }
 
  export default ListContacts
