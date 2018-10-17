afe9a36ac0e4af32bc8925de5e171e7bdabb70de
Tagwa-Esam <tagwaesam5@gmail.com>
2018-10-17 14:09:53 -0400

    Save new contact to the server

--- src/App.js
+++ src/App.js
@@ -24,6 +24,15 @@ class App extends Component {
     //remove contact from the data base
     ContactsAPI.remove(contact)
   }
+
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
       <div>
@@ -34,8 +43,14 @@ class App extends Component {
 
         />
       )}/>
-        <Route path='/create' component={CreateContact}/>
-
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
     )
   }
